5863384505147a776c4b563bbd5d87a6b938098c
asagiman <45725946+suirindo@users.noreply.github.com>
2023-05-04 08:00:26 +0000

    mjs base 修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -20,8 +20,8 @@ export default defineConfig({
       noExternal: ['swiper', 'leaflet'],
     },
   },
-  site: 'https://hellotham.github.io',
-  base: '/hello-astro',
+  site: 'https://netsujo.github.io',
+  base: '/netsujo-corp',
   integrations: [tailwind(), sitemap(), image(), mdx(), alpinejs(), robotsTxt()],
   experimental: {
     integrations: true,
